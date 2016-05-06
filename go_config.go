4ae8e11612d4570589a762717b9657a4a40a4343
James <james@mercstudio.com>
2016-05-06 23:02:51 +0800

    fixed fallback section

--- go_config.go
+++ go_config.go
@@ -1,11 +1,11 @@
 package go_config
 
 import (
-	// "github.com/astaxie/beego"
-	// "github.com/astaxie/beego/config"
 	"github.com/go-ini/ini"
 	"fmt"
 	"strings"
+	"os"
+	"path/filepath"
 )
 
 type IniConfigLoader struct {
@@ -15,16 +15,39 @@ type IniConfigLoader struct {
 
 func NewConfigLoader(_type string, file string) (*IniConfigLoader, error) {
   if(_type == "ini") {
+		path, err := filepath.Abs(file)
+		if (err != nil) {
+			err_f := fmt.Errorf("unable to get abs path: %s", file)
+			return nil, err_f
+		}
+		fmt.Printf("path: %s\n", path)
+		_, err = os.Stat(path);
+		if  err != nil {
+			fmt.Printf("ERROR!!!!: %s\n", err.Error())
+		  return nil, err
+		}
     config := new(IniConfigLoader)
-  	_, err := config.load(file)
+  	_, err = config.load(file)
 		return config, err
   } else {
     return nil, fmt.Errorf("NewConfigLoader: unsupported format: %s", _type)
   }
 }
 
-func (this *IniConfigLoader) GetSection(name string) (*ini.Section, error) {
-	return this.loader.GetSection(name)
+func (this *IniConfigLoader) Section(name string) *ini.Section {
+	// fmt.Printf("loader config %v", this)
+	section, err := this.loader.GetSection(name)
+	if err != nil {
+		section, err = this.loader.GetSection("")
+		if err != nil {
+			err_f := fmt.Errorf("Section %s does not exists", name)
+			fmt.Printf(err_f.Error())
+			return nil
+		}
+		fmt.Printf("section fallback to root for %s", name)
+		return section
+	}
+	return section
 }
 
 func (this *IniConfigLoader) load(filename string) (*ini.File, error) {
@@ -35,33 +58,34 @@ func (this *IniConfigLoader) load(filename string) (*ini.File, error) {
 }
 
 func (this *IniConfigLoader) String(section string, key string, default_value string) string {
-	if this.loader.Section(section).HasKey(key) {
-		return this.loader.Section(section).Key(key).String()
-	} else if this.loader.Section("").HasKey(key) {
-		return this.loader.Section("").Key(key).String()
+	// fmt.Printf("Getting string: %s > %s\n", section, key)
+	if this.Section(section).HasKey(key) {
+		return this.Section(section).Key(key).MustString(default_value)
+	} else if (this.loader.Section("").HasKey(key)) {
+		return this.loader.Section("").Key(key).MustString(default_value)
 	} else {
 		return default_value
-	}	
+	}
 }
 
 func (this *IniConfigLoader) Int(section string, key string, default_value int) int {
-	if this.loader.Section(section).HasKey(key) {
-		return this.loader.Section(section).Key(key).MustInt(default_value)
-	} else if this.loader.Section("").HasKey(key) {
-		return this.loader.Section("").Key(key).MustInt(default_value)
+	if this.Section(section).HasKey(key) {
+		return this.Section(section).Key(key).MustInt(default_value)
+	} else if (this.loader.Section("").HasKey(key)) {
+		return this.loader.Section("").Key(key).MustInt()
 	} else {
-		return (default_value)
-	}	
+		return default_value
+	}
 }
 
 func (this *IniConfigLoader) Float(section string, key string, default_value float64) float64 {
-	if this.loader.Section(section).HasKey(key) {
-		return this.loader.Section(section).Key(key).MustFloat64(default_value)
-	} else if this.loader.Section("").HasKey(key) {
-		return this.loader.Section("").Key(key).MustFloat64(default_value)
+	if this.Section(section).HasKey(key) {
+		return this.Section(section).Key(key).MustFloat64(default_value)
+	} else if (this.loader.Section("").HasKey(key)) {
+		return this.loader.Section("").Key(key).MustFloat64()
 	} else {
 		return default_value
-	}	
+	}
 }
 
 func (this *IniConfigLoader) Boolean(section string, key string, default_value bool) bool {
@@ -70,7 +94,7 @@ func (this *IniConfigLoader) Boolean(section string, key string, default_value b
 		default_string = "true"
 	}
 	value := this.String(section, key, default_string)
-	
+
 	if(strings.ToLower(value) == "true"){
 		return true
 	} else {
